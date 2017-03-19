287258b41a5715bb0399f52cb45ddf427f4a17ae
Anthony Hurst <Anthony.Hurst@gmail.com>
2017-03-19 18:38:24 +0000

    PokerCpp: Added less than operator
    Implemented the less than operator.  It's used for all the other
    inequality operators so it's pretty important.  Did not test or try
    it yet (as I dont have actual hand values yet).

--- hand_value.cpp
+++ hand_value.cpp
@@ -31,15 +31,13 @@ bool HandValue::operator==(const HandValue& handValue) {
     if (hand_type_ != handValue.GetHandType())
         return false;
     const int* otherSubValue = handValue.GetSubValue();
-    for (int i = 0; i < SUBVALUESIZE; ++i) {
+    for (int i = 0; i < SUBVALUESIZE; ++i)
         if (sub_value_[i] != otherSubValue[i])
             return false;
-    }
     const int* kickerValue = handValue.GetKickerValue();
-    for (int i = 0; i < SUBVALUESIZE; ++i) {
+    for (int i = 0; i < SUBVALUESIZE; ++i)
         if (kicker_value_[i] != kickerValue[i])
             return false;
-    }
     return true;
 }
 
@@ -48,7 +46,26 @@ bool HandValue::operator!=(const HandValue& handValue) {
 }
 
 bool HandValue::operator<(const HandValue& handValue) {
-    // to be implemented
+    // we should only need to 'keep going' if things are 
+    // equivalent so we just need to check for the 
+    // inequality types to short circuit.
+    if (hand_type_ < handValue.GetHandType())
+        return true;
+    else if (hand_type_ > handValue.GetHandType())
+        return false;
+    const int* otherSubValue = handValue.GetSubValue();
+    for (int i = 0; i < SUBVALUESIZE; ++i)
+        if (sub_value_[i] < otherSubValue[i]) 
+            return true;
+        else if (sub_value_[i] > otherSubValue[i])
+            return false;
+    const int* otherKickerValue = handValue.GetKickerValue();
+    for (int i = 0; i < SUBVALUESIZE; ++i)
+        if (sub_value_[i] < otherKickerValue[i]) 
+            return true;
+        else if (sub_value_[i] > otherKickerValue[i])
+            return false;
+    return false; // they must be equal and we return strictly "<"
 }
 
 bool HandValue::operator>(const HandValue& handValue) {
